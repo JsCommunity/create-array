321cf290a9362ba1ecbd14487f84257bbe3075d1
Julien Fontanet <julien.fontanet@isonoe.net>
2016-09-27 14:46:07 +0200

    chore(package): use standard

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-'use strict';
+'use strict'
 
-//====================================================================
+// ===================================================================
 
 var isFunction = (function (toString) {
-  var tag = toString.call(toString);
-  return function isFunction(val) {
-    return toString.call(val) === tag;
-  };
-})(Object.prototype.toString);
+  var tag = toString.call(toString)
+  return function isFunction (val) {
+    return toString.call(val) === tag
+  }
+})(Object.prototype.toString)
 
-function wrapValue(value) {
-  return function wrappedValue() {
-    return value;
-  };
+function wrapValue (value) {
+  return function wrappedValue () {
+    return value
+  }
 }
 
-//--------------------------------------------------------------------
+// -------------------------------------------------------------------
 
-module.exports = function createArray(n, generator) {
-  var i, ar;
-  ar = new Array(n);
+module.exports = function createArray (n, generator) {
+  var i, ar
+  ar = new Array(n)
 
   if (!isFunction(generator)) {
-    generator = wrapValue(generator);
+    generator = wrapValue(generator)
   }
 
   for (i = 0; i < n; ++i) {
-    ar[i] = generator(i);
+    ar[i] = generator(i)
   }
 
-  return ar;
-};
+  return ar
+}
