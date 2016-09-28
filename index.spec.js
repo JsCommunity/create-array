93d28ec56bc97a588c85141967f8c427c3165d67
Julien Fontanet <julien.fontanet@isonoe.net>
2016-09-28 18:01:16 +0200

    chore(tests): 5 items is more than enough

--- index.spec.js
+++ index.spec.js
@@ -17,18 +17,16 @@ describe('createArray()', function () {
   })
 
   it('works with non function generator', function () {
-    expect(createArray(10, 'foo')).to.deep.equal([
-      'foo', 'foo', 'foo', 'foo', 'foo',
+    expect(createArray(5, 'foo')).to.deep.equal([
       'foo', 'foo', 'foo', 'foo', 'foo'
     ])
   })
 
   it('works with function generator', function () {
-    expect(createArray(10, function (i) {
+    expect(createArray(5, function (i) {
       return 'item #' + i
     })).to.deep.equal([
-      'item #0', 'item #1', 'item #2', 'item #3', 'item #4',
-      'item #5', 'item #6', 'item #7', 'item #8', 'item #9'
+      'item #0', 'item #1', 'item #2', 'item #3', 'item #4'
     ])
   })
 })
