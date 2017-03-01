e2a1de8407baecb08be7d84cf557232f9091954a
Julien Fontanet <julien.fontanet@isonoe.net>
2017-03-01 10:16:24 +0100

    chore: use jest for testing

--- index.spec.js
+++ index.spec.js
@@ -1,23 +1,22 @@
-/* eslint-env mocha */
+/* eslint-env jest */
 
 'use strict'
 
 // ====================================================================
 
 var createArray = require('./')
-var expect = require('chai').expect
 
 // ====================================================================
 
 describe('createArray()', function () {
   it('default generator use the index', function () {
-    expect(createArray(5)).to.deep.equal([
+    expect(createArray(5)).toEqual([
       0, 1, 2, 3, 4
     ])
   })
 
   it('works with non function generator', function () {
-    expect(createArray(5, 'foo')).to.deep.equal([
+    expect(createArray(5, 'foo')).toEqual([
       'foo', 'foo', 'foo', 'foo', 'foo'
     ])
   })
@@ -25,7 +24,7 @@ describe('createArray()', function () {
   it('works with function generator', function () {
     expect(createArray(5, function (i) {
       return 'item #' + i
-    })).to.deep.equal([
+    })).toEqual([
       'item #0', 'item #1', 'item #2', 'item #3', 'item #4'
     ])
   })
