cfd0242d416d2994c30ef84b9b296f3c4eddf463
Julien Fontanet <julien.fontanet@isonoe.net>
2018-06-25 11:08:20 +0200

    chore: format tests with Prettier

--- index.spec.js
+++ index.spec.js
@@ -10,22 +10,18 @@ var createArray = require('./')
 
 describe('createArray()', function () {
   it('default generator use the index', function () {
-    expect(createArray(5)).toEqual([
-      0, 1, 2, 3, 4
-    ])
+    expect(createArray(5)).toEqual([0, 1, 2, 3, 4])
   })
 
   it('works with non function generator', function () {
-    expect(createArray(5, 'foo')).toEqual([
-      'foo', 'foo', 'foo', 'foo', 'foo'
-    ])
+    expect(createArray(5, 'foo')).toEqual(['foo', 'foo', 'foo', 'foo', 'foo'])
   })
 
   it('works with function generator', function () {
-    expect(createArray(5, function (i) {
-      return 'item #' + i
-    })).toEqual([
-      'item #0', 'item #1', 'item #2', 'item #3', 'item #4'
-    ])
+    expect(
+      createArray(5, function (i) {
+        return 'item #' + i
+      })
+    ).toEqual(['item #0', 'item #1', 'item #2', 'item #3', 'item #4'])
   })
 })
